9bcac972310091daf60355838c9cba15c574f54e
sandeep katta <sandeep.katta2007@gmail.com>
2019-08-21 20:09:24 +0530

    Fix the class loader issue

--- sql/hive-thriftserver/src/main/scala/org/apache/spark/sql/hive/thriftserver/SparkSQLCLIDriver.scala
+++ sql/hive-thriftserver/src/main/scala/org/apache/spark/sql/hive/thriftserver/SparkSQLCLIDriver.scala
@@ -94,6 +94,7 @@ private[hive] object SparkSQLCLIDriver extends Logging {
         cliConf.set(k, v)
     }
 
+    val orginalClassLoader = Thread.currentThread().getContextClassLoader
     val sessionState = new CliSessionState(cliConf)
 
     sessionState.in = System.in
@@ -139,7 +140,7 @@ private[hive] object SparkSQLCLIDriver extends Logging {
       // Hadoop-20 and above - we need to augment classpath using hiveconf
       // components.
       // See also: code in ExecDriver.java
-      var loader = conf.getClassLoader
+      var loader = orginalClassLoader
       val auxJars = HiveConf.getVar(conf, HiveConf.ConfVars.HIVEAUXJARS)
       if (StringUtils.isNotBlank(auxJars)) {
         loader = ThriftserverShimUtils.addToClassPath(loader, StringUtils.split(auxJars, ","))
